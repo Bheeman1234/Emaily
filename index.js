12c939ac760b69df32954211056f31043a6b46cd
Bheeman1234 <thejavathbheeman@gmail.com>
2025-09-17 16:16:10 +0530

    index change

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ if (process.env.NODE_ENV === 'production') {
 
     // Express will serve up the index.html file
     // if it doesn't recognize the route
-    const path = require('path');
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    const root = require("path").join(__dirname, "./build");
+    app.use(express.static(root))
+    app.get("*", (req, res) => {
+        res.sendFile("index.html", {root});
     });
 }
 
